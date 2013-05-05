6bebd662e8d19908702fa66f4c6a8ae381a02364
soncy <cs1900@gmail.com>
2013-05-05 14:47:36 +0800

    add wholesaleinternet.net $19 server monitor

---
+++ wholesaleinternet.net.js
@@ -0,0 +1,33 @@
+/*
+ * Created with Sublime Text (buidl 3031).
+ * User: soncy
+ * Date: 2013-05-05
+ * Time: 14:29:40
+ * Contact: http://www.qifendi.com
+ */
+
+var ssmmonitor = require('./lib/ssmmonitor'),
+    $ = require('jquery').create();
+
+var config = {
+    name: '19刀', 
+    url: 'https://www.wholesaleinternet.net/dedicated/',
+    interval: 60,
+    checkFunc: function(data) {
+        return checkAvailable(data);
+    },
+    mailSubject: '19刀有货啦'
+};
+
+function checkAvailable(data) {
+    var html = $(data),
+        pricearea = html.find('.pricearea');
+
+    for (var i = 0; i < 3; i++) { // 判断两个19刀和一个20刀
+        if (!(~pricearea.html().indexOf('Out Of Stock'))) {
+            return true;
+        }
+    }
+}
+
+ssmmonitor.start(config);
