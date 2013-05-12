df94ea664343fac9d468a9416f1e9f88fa349f9b
soncy <cs1900@gmail.com>
2013-05-12 10:53:36 +0800

    add wholesaleinternet 19 dollar monitor

---
+++ wholesaleinternetatom.net.js
@@ -0,0 +1,25 @@
+/*
+ * Created with Sublime Text (buidl 3032).
+ * User: soncy
+ * Date: 2013-05-12
+ * Time: 10:52:37
+ * Contact: http://www.qifendi.com
+ */
+
+var ssmmonitor = require('./lib/ssmmonitor');
+
+var config = {
+    name: 'wholesaleinternet', 
+    url: 'https://www.wholesaleinternet.net/cart/?id=215',
+    interval: 120,
+    checkFunc: function(data) {
+        return checkAvailable(data);
+    },
+    mailSubject: '19刀有货啦'
+};
+
+function checkAvailable(data) {
+    return !(~data.indexOf('out of stock'));
+}
+
+ssmmonitor.start(config);
---
+++ wholesaleinternetp4.net.js
@@ -0,0 +1,26 @@
+/*
+ * Created with Sublime Text (buidl 3032).
+ * User: soncy
+ * Date: 2013-05-12
+ * Time: 10:51:11
+ * Contact: http://www.qifendi.com
+ */
+
+
+var ssmmonitor = require('./lib/ssmmonitor');
+
+var config = {
+    name: 'wholesaleinternet', 
+    url: 'https://www.wholesaleinternet.net/cart/?id=214',
+    interval: 120,
+    checkFunc: function(data) {
+        return checkAvailable(data);
+    },
+    mailSubject: '19刀有货啦'
+};
+
+function checkAvailable(data) {
+    return !(~data.indexOf('out of stock'));
+}
+
+ssmmonitor.start(config);
